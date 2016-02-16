b6ef02affcf316b0bda81677b2d410596f2ddfc4
Jay Hayes <ur@iamvery.com>
2016-02-16 15:04:29 -0600

    solutions

--- about_lists.exs
+++ about_lists.exs
@@ -2,37 +2,37 @@ defmodule AboutLists do
   use Koans
 
   think "Create your first list" do
-    a_list = __?
+    a_list = []
     assert is_list(a_list)
   end
 
   think "Getting list length is a kernel feature" do
     a_list = [1, 2, 3]
-    assert length(a_list) == __?
+    assert length(a_list) == 3
   end
 
   think "Elixir provide special operator to concatenate lists" do
     a_list = [1, 2]
-    assert a_list ++ [3] == __?
+    assert a_list ++ [3] == [1,2,3]
   end
 
   think "Elixir provide special operator to remove element from list" do
     a_list = [1, 2, 3]
-    assert a_list -- [2] == __?
+    assert a_list -- [2] == [1,3]
   end
 
   think "Only first element is removed with truncate operator" do
     a_list = [:foo, :bar, :foo]
-    assert a_list -- [:foo] == __?
+    assert a_list -- [:foo] == [:bar, :foo]
   end
 
   think "Truncate operator do nothing when element not in list" do
     a_list = [:foo, :bar]
-    assert_? a_list -- [:baz] == [:foo, :bar]
+    assert a_list -- [:baz] == [:foo, :bar]
   end
 
   think "The in operator test if element is present inner an enum" do
     a_list = [:foo, :bar]
-    assert_? :bar in a_list
+    assert :bar in a_list
   end
 end
